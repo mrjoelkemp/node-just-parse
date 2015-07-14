8ca66e05ad19bab2b6dc92079900b1b9612bdefe
Joel Kemp <mrjoelkemp@gmail.com>
2015-07-14 08:41:53 -0400

    Add test to ensure scripts are parsed in addition to modules

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var justParse = require('../');
 
 var assert = require('assert');
 var fs = require('fs');
+var sinon = require('sinon');
 
 describe('just-parse', function() {
   it('ignores unparsable .js files', function() {
@@ -20,5 +21,16 @@ describe('just-parse', function() {
     var filePath = __dirname + '/example/valid.js';
     var ast = justParse(fs.readFileSync(filePath, 'utf8'));
     assert(ast instanceof Object);
+  });
+
+  it('should parse scripts even with a source type of module', function() {
+    var stub = sinon.stub(justParse, '_getParserOptions').returns({
+      ecmaVersion: 6,
+      sourceType: 'module'
+    });
+
+    var ast = justParse('#!/usr/bin/env node\nvar foo = 1;');
+    assert(ast instanceof Object);
+    stub.restore();
   });
 });
